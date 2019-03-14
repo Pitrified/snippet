5b38866706eac237db840ce3e1e9008015a3c5a5
Pitrified <nobilipietro@gmail.com>
2019-03-15 00:19:46 +0100

    A lot of colors

---
+++ terminal-colors/README.md
@@ -0,0 +1,12 @@
+### Colors with ANSI control sequences
+
+Very complete [guide](https://misc.flogisoft.com/bash/tip_colors_and_formatting)
+
+With [SO](https://stackoverflow.com/a/33206814) you can't go wrong
+
+[Dude](https://askubuntu.com/a/279014)
+
+Something more [abstract](https://wynnnetherland.com/journal/a-stylesheet-author-s-guide-to-terminal-colors/) from codes
+#### Explanation
+
+Template for control sequence: `<ESC>[{FORMATCODE}m`
--- terminal-colors/colors.py
+++ terminal-colors/colors.py
@@ -4,9 +4,34 @@ def main():
     cs = 'echo -n "\\033[{}m{}\\033[0m"'
     system(cs.format(32, 'Hi\n'))
 
+    cs = r'echo -n "\e[{code}m{chars}\e[0m"'
+    system(cs.format(code=4, chars='Hi\n'))
+
+    print("\033[31;1;4mHello\033[0m")
+    print("\033[31;1;4;3;9mHello\033[0m")
+    print("\033[31;1;4;3mHello\033[0m")
+    print("\033[31;1;4mHello\033[0m")
+    cs = "\033[{}m{}\033[0m"
+
+    styles = range(0,8+1)
+    colors = list(range(30,37+1))+list(range(90,97+1))
+    backgrounds = list(range(40,47+1)) + list(range(100,107+1))
+    for s in styles:
+        for c in colors:
+            for b in backgrounds:
+                code = f'{s};{c};{b}'
+                print(cs.format(code, code) , end='')
+                print(' ' , end='')
+            print()
+        print()
+
+
+
 if __name__ == '__main__':
     main()
 
+# NOTE
+# on a terminal, echo -e is needed for it to parse escape sequences
 
 # More info:
 # martin-thoma.com/colorize-your-scripts-output/
