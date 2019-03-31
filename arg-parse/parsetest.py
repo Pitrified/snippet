a83c482d481b29a5c94db5be22b83a04b80b7c39
Pitrified <nobilipietro@gmail.com>
2019-03-31 21:06:51 +0200

    Tidy up

--- arg-parse/parsetest.py
+++ arg-parse/parsetest.py
@@ -3,8 +3,6 @@ import argparse
 # https://docs.python.org/dev/howto/argparse.html
 
 def parse_arguments():
-    print('Parsing...')
-
     # setup parser
     parser = argparse.ArgumentParser(
             description='Short argparse usage example',
@@ -22,14 +20,14 @@ def parse_arguments():
             action="store_true",                # store only true or false value
             help="increase output verbosity")
 
-    # limit flag options
+    # limit possible values
     parser.add_argument('-n', '--number',
             type=int,
             choices=[0,1,2],
             help='number can only be 0,1,2',
             )
 
-    # fancy flag with count
+    # fancy option with count
     parser.add_argument('-r', '--repeat',
             action='count',
             default=0,
@@ -38,14 +36,11 @@ def parse_arguments():
 
     # last line to parse them
     args = parser.parse_args()
-    #  print(args._get_kwargs() )
-    # parse the args even more if needed
+
+    #  parse the args even more if needed
     args_parsed = { a : v for a, v in args._get_kwargs() }
     return args_parsed
 
-#  def main(*args, **kwargs):
-#  def main(*args):
-#  def main(args): print(f'{args}')
 def main(x, y, echo, square, verbose, number, repeat=1):
     print(f'echo: {echo}')
     print(f'repeat: {repeat}')
@@ -53,14 +48,7 @@ def main(x, y, echo, square, verbose, number, repeat=1):
 if __name__ == '__main__':
     arguments = parse_arguments()
     print(f'{arguments}')
+
     myx = 9
     myy = 3
     main(myx, myy, **arguments)
-    echo='here'
-    square = 4
-    verbose = True
-    number = 5
-    repeat = 4
-    main(myx, myy, echo, square, verbose, number, repeat)
-    main(myx, myy, echo, square, verbose, number)
-
