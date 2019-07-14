56cb02ed3a4d13acd56eb57cc29bc72ba3bb19eb
Pitrified <nobilipietro@gmail.com>
2019-07-14 10:13:52 +0200

    Add guards to load logger only once
    Black the code

--- bresenham/bresenham_line.py
+++ bresenham/bresenham_line.py
@@ -5,32 +5,33 @@ import numpy as np
 
 from timeit import default_timer as timer
 
+
 class Bresenham:
-    '''Draw a line using the Bresenham algorithm
+    """Draw a line using the Bresenham algorithm
 
     https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
-    '''
-    def __init__(self,
-            width,
-            height,
-            ):
-        '''Initialize a Bresenham map
-        '''
+    """
+
+    class_logger_loaded = False
+
+    def __init__(self, width, height):
+        """Initialize a Bresenham map
+        """
 
         self.setup_logger()
 
         self.width = width
         self.height = height
 
-        self.map = np.zeros( (self.width, self.height), dtype=np.uint8 )
+        self.map = np.zeros((self.width, self.height), dtype=np.uint8)
 
     def add_line(self, x0, y0, x1, y1):
-        '''Add a line, going from (x0, y0) to (x1, y1)
-        '''
-        logline = logging.getLogger(f'{__class__.__name__}.console.linea')
+        """Add a line, going from (x0, y0) to (x1, y1)
+        """
+        logline = logging.getLogger(f"{__class__.__name__}.console.linea")
         #  logline.setLevel('DEBUG')
-        logline.setLevel('INFO')
-        logline.debug('LINE')
+        logline.setLevel("INFO")
+        logline.debug("LINE")
 
         self.map[x0, y0] = 1
         self.map[x1, y1] = 1
@@ -47,12 +48,12 @@ class Bresenham:
                 self.add_line_high(x0, y0, x1, y1)
 
     def add_line_low(self, x0, y0, x1, y1):
-        '''
-        '''
-        logline = logging.getLogger(f'{__class__.__name__}.console.linel')
+        """
+        """
+        logline = logging.getLogger(f"{__class__.__name__}.console.linel")
         #  logline.setLevel('DEBUG')
-        logline.setLevel('INFO')
-        logline.debug('LOW')
+        logline.setLevel("INFO")
+        logline.debug("LOW")
 
         dx = x1 - x0
         dy = y1 - y0
@@ -62,25 +63,25 @@ class Bresenham:
             yi = -1
             dy = -dy
 
-        D = 2*dy - dx
+        D = 2 * dy - dx
         y = y0
 
-        for x in range(x0, x1+1):
-            logline.log(5, f'Mapping {x} {y}')
+        for x in range(x0, x1 + 1):
+            logline.log(5, f"Mapping {x} {y}")
             self.map[x, y] = 2
             if D > 0:
-                 y = y + yi
-                 D = D - 2*dx
-            D = D + 2*dy
+                y = y + yi
+                D = D - 2 * dx
+            D = D + 2 * dy
 
     def add_line_high(self, x0, y0, x1, y1):
-        '''
-        '''
-        logline = logging.getLogger(f'{__class__.__name__}.console.lineh')
-        logline.setLevel('INFO')
+        """
+        """
+        logline = logging.getLogger(f"{__class__.__name__}.console.lineh")
+        logline.setLevel("INFO")
         #  logline.setLevel('DEBUG')
         #  logline.setLevel('TRACE')
-        logline.debug('HIGH')
+        logline.debug("HIGH")
 
         dx = x1 - x0
         dy = y1 - y0
@@ -88,48 +89,50 @@ class Bresenham:
         if dx < 0:
             xi = -1
             dx = -dx
-        D = 2*dx - dy
+        D = 2 * dx - dy
         x = x0
 
-        for y in range(y0, y1+1):
-            logline.log(5, f'Mapping {x} {y}')
+        for y in range(y0, y1 + 1):
+            logline.log(5, f"Mapping {x} {y}")
             self.map[x, y] = 2
             if D > 0:
-                 x = x + xi
-                 D = D - 2*dy
-            D = D + 2*dx
+                x = x + xi
+                D = D - 2 * dy
+            D = D + 2 * dx
 
     def getstr_map_noc(self):
-        '''Return the map as a string
-        '''
-        map_noc = ''
+        """Return the map as a string
+        """
+        map_noc = ""
         #  fmt = '{:02d}'
-        fmt = '{:01d}'
+        fmt = "{:01d}"
         for line in self.map:
             for n in line:
                 map_noc += fmt.format(n)
-            map_noc += '\n'
+            map_noc += "\n"
         return map_noc
 
-    def setup_logger(self, logLevel='DEBUG'):
-        '''Setup logger that outputs to console for the module
-        '''
-        logmoduleconsole = logging.getLogger(f'{__class__.__name__}.console')
-        logmoduleconsole.propagate = False
-        logmoduleconsole.setLevel(logLevel)
+    def setup_logger(self, logLevel="DEBUG"):
+        """Setup logger that outputs to console for the module
+        """
+        if Bresenham.class_logger_loaded is False:
+            logmoduleconsole = logging.getLogger(f"{__class__.__name__}.console")
+            logmoduleconsole.propagate = False
+            logmoduleconsole.setLevel(logLevel)
 
-        module_console_handler = logging.StreamHandler()
+            module_console_handler = logging.StreamHandler()
 
-        #  log_format_module = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-        log_format_module = '%(name)s - %(levelname)s: %(message)s'
-        #  log_format_module = '%(levelname)s: %(message)s'
-        formatter = logging.Formatter(log_format_module)
-        module_console_handler.setFormatter(formatter)
+            #  log_format_module = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+            log_format_module = "%(name)s - %(levelname)s: %(message)s"
+            #  log_format_module = '%(levelname)s: %(message)s'
+            formatter = logging.Formatter(log_format_module)
+            module_console_handler.setFormatter(formatter)
 
-        logmoduleconsole.addHandler(module_console_handler)
+            logmoduleconsole.addHandler(module_console_handler)
 
-        logging.addLevelName(5, 'TRACE')
-        # use it like this
-        # logmoduleconsole.log(5, 'Exceedingly verbose debug')
+            logging.addLevelName(5, "TRACE")
+            # use it like this
+            # logmoduleconsole.log(5, 'Exceedingly verbose debug')
 
-        return logmoduleconsole
+            Bresenham.class_logger_loaded = True
+            return logmoduleconsole
--- bresenham/bresenham_main.py
+++ bresenham/bresenham_main.py
@@ -8,119 +8,124 @@ from timeit import default_timer as timer
 
 from bresenham_line import Bresenham
 
+
 def parse_arguments():
-    '''Setup CLI interface
-    '''
-    parser = argparse.ArgumentParser(
-            description='',
-            )
+    """Setup CLI interface
+    """
+    parser = argparse.ArgumentParser(description="")
 
-    parser.add_argument('-i', "--input_path",
-            type=str,
-            default='hp.jpg',
-            help="path to input image to use")
+    parser.add_argument(
+        "-i",
+        "--input_path",
+        type=str,
+        default="hp.jpg",
+        help="path to input image to use",
+    )
 
-    parser.add_argument('-s', "--seed",
-            type=int,
-            default=-1,
-            help="random seed to use")
+    parser.add_argument("-s", "--seed", type=int, default=-1, help="random seed to use")
 
     # last line to parse the args
     args = parser.parse_args()
     return args
 
-def setup_logger(logLevel='DEBUG'):
-    '''Setup logger that outputs to console for the module
-    '''
-    logmoduleconsole = logging.getLogger(f'{__name__}.console')
+
+def setup_logger(logLevel="DEBUG"):
+    """Setup logger that outputs to console for the module
+    """
+    logmoduleconsole = logging.getLogger(f"{__name__}.console")
     logmoduleconsole.propagate = False
     logmoduleconsole.setLevel(logLevel)
 
     module_console_handler = logging.StreamHandler()
 
     #  log_format_module = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    log_format_module = '%(name)s - %(levelname)s: %(message)s'
+    log_format_module = "%(name)s - %(levelname)s: %(message)s"
     #  log_format_module = '%(levelname)s: %(message)s'
     formatter = logging.Formatter(log_format_module)
     module_console_handler.setFormatter(formatter)
 
     logmoduleconsole.addHandler(module_console_handler)
 
-    logging.addLevelName(5, 'TRACE')
+    logging.addLevelName(5, "TRACE")
     # use it like this
     # logmoduleconsole.log(5, 'Exceedingly verbose debug')
 
     return logmoduleconsole
 
-def test_creation(width, height, logLevel='INFO'):
-    '''Create an instance of Bresenham
-    '''
-    logcreate = logging.getLogger(f'{__name__}.console.create')
+
+def test_creation(width, height, logLevel="INFO"):
+    """Create an instance of Bresenham
+    """
+    logcreate = logging.getLogger(f"{__name__}.console.create")
     logcreate.setLevel(logLevel)
 
     bre = Bresenham(width, height)
-    logcreate.debug('\n' + bre.getstr_map_noc())
+    logcreate.debug("\n" + bre.getstr_map_noc())
 
-def test_line(width, height, x0, y0, x1, y1, logLevel='INFO'):
 
-    logline = logging.getLogger(f'{__name__}.console.line')
+def test_line(width, height, x0, y0, x1, y1, logLevel="INFO"):
+
+    logline = logging.getLogger(f"{__name__}.console.line")
     logline.setLevel(logLevel)
 
     bre = Bresenham(width, height)
 
-    bre.add_line( x0, y0, x1, y1 )
+    bre.add_line(x0, y0, x1, y1)
 
-    logline.debug('\n' + bre.getstr_map_noc())
+    logline.debug("\n" + bre.getstr_map_noc())
 
-def test_lines(width, height, logLevel='INFO'):
 
-    logline = logging.getLogger(f'{__name__}.console.lines')
+def test_lines(width, height, logLevel="INFO"):
+
+    logline = logging.getLogger(f"{__name__}.console.lines")
     logline.setLevel(logLevel)
 
     bre = Bresenham(width, height)
 
-    x0, y0 = (10,10)
+    x0, y0 = (10, 10)
 
-    x1, y1 = (14,18)
-    logline.debug(f'Line ({x0}, {y0}) to ({x1}, {y1})')
-    bre.add_line( x0, y0, x1, y1 )
-    x1, y1 = (18,14)
-    logline.debug(f'Line ({x0}, {y0}) to ({x1}, {y1})')
-    bre.add_line( x0, y0, x1, y1 )
+    x1, y1 = (14, 18)
+    logline.debug(f"Line ({x0}, {y0}) to ({x1}, {y1})")
+    bre.add_line(x0, y0, x1, y1)
+    x1, y1 = (18, 14)
+    logline.debug(f"Line ({x0}, {y0}) to ({x1}, {y1})")
+    bre.add_line(x0, y0, x1, y1)
 
-    x1, y1 = (6,18)
-    logline.debug(f'Line ({x0}, {y0}) to ({x1}, {y1})')
-    bre.add_line( x0, y0, x1, y1 )
-    x1, y1 = (2,14)
-    logline.debug(f'Line ({x0}, {y0}) to ({x1}, {y1})')
-    bre.add_line( x0, y0, x1, y1 )
+    x1, y1 = (6, 18)
+    logline.debug(f"Line ({x0}, {y0}) to ({x1}, {y1})")
+    bre.add_line(x0, y0, x1, y1)
+    x1, y1 = (2, 14)
+    logline.debug(f"Line ({x0}, {y0}) to ({x1}, {y1})")
+    bre.add_line(x0, y0, x1, y1)
 
-    x1, y1 = (2,8)
-    logline.debug(f'Line ({x0}, {y0}) to ({x1}, {y1})')
-    bre.add_line( x0, y0, x1, y1 )
-    x1, y1 = (6,2)
-    logline.debug(f'Line ({x0}, {y0}) to ({x1}, {y1})')
-    bre.add_line( x0, y0, x1, y1 )
+    x1, y1 = (2, 8)
+    logline.debug(f"Line ({x0}, {y0}) to ({x1}, {y1})")
+    bre.add_line(x0, y0, x1, y1)
+    x1, y1 = (6, 2)
+    logline.debug(f"Line ({x0}, {y0}) to ({x1}, {y1})")
+    bre.add_line(x0, y0, x1, y1)
 
-    x1, y1 = (14,2)
-    logline.debug(f'Line ({x0}, {y0}) to ({x1}, {y1})')
-    bre.add_line( x0, y0, x1, y1 )
-    x1, y1 = (18,8)
-    logline.debug(f'Line ({x0}, {y0}) to ({x1}, {y1})')
-    bre.add_line( x0, y0, x1, y1 )
+    x1, y1 = (14, 2)
+    logline.debug(f"Line ({x0}, {y0}) to ({x1}, {y1})")
+    bre.add_line(x0, y0, x1, y1)
+    x1, y1 = (18, 8)
+    logline.debug(f"Line ({x0}, {y0}) to ({x1}, {y1})")
+    bre.add_line(x0, y0, x1, y1)
 
-    logline.info('\n' + bre.getstr_map_noc())
+    logline.info("\n" + bre.getstr_map_noc())
+
 
 def test_bresenham():
-    '''Test Bresenham algorithm
-    '''
-    #  test_creation(10, 20, 'DEBUG')
-    x0, y0 = (0,0)
-    x1, y1 = (4,8)
+    """Test Bresenham algorithm
+    """
+    test_creation(10, 20, "DEBUG")
+    x0, y0 = (0, 0)
+    x1, y1 = (4, 8)
     #  test_line(10, 10, x0, y0, x1, y1, 'DEBUG')
 
-    #  test_lines(20, 20, 'DEBUG')
-    test_lines(20, 20, 'INFO')
+    test_lines(20, 20, "DEBUG")
+    #  test_lines(20, 20, 'INFO')
+
 
 def main():
     args = parse_arguments()
@@ -128,7 +133,7 @@ def main():
     # setup seed value
     if args.seed == -1:
         myseed = 1
-        myseed = int( timer() * 1e9 % 2**32 )
+        myseed = int(timer() * 1e9 % 2 ** 32)
     else:
         myseed = args.seed
     seed(myseed)
@@ -138,9 +143,10 @@ def main():
 
     logmoduleconsole = setup_logger()
 
-    logmoduleconsole.info(f'python3 bresenham_main.py -s {myseed} -i {path_input}')
+    logmoduleconsole.info(f"python3 bresenham_main.py -s {myseed} -i {path_input}")
 
     test_bresenham()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
