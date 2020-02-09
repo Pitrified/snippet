e38750ff805844be4cba61e13cbe353627ac1fba
Pitrified <nobilipietro@gmail.com>
2020-02-09 18:24:56 +0100

    [DJANGO] Add basic css

--- django-tutorial/blog/views.py
+++ django-tutorial/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+from .models import BlogPost
+
+
+def post_list(request):
+
+    posts = BlogPost.objects.filter(published_date__lte=timezone.now()).order_by(
+        "published_date"
+    )
+
+    return render(request, "blog/post_list.html", {'posts':posts})
