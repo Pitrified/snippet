f5009052ce1d1b5b460b5bbe1a0a6a2b3c0fa12c
Pitrified <nobilipietro@gmail.com>
2020-02-10 01:34:24 +0100

    [DJANGO] Add form for new post

---
+++ django-tutorial/blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import BlogPost
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = BlogPost
+        fields = (
+            "title",
+            "text",
+        )
--- django-tutorial/blog/views.py
+++ django-tutorial/blog/views.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 
 from .models import BlogPost
 
+from .forms import PostForm
+
 
 def post_list(request):
     posts = BlogPost.objects.filter(published_date__lte=timezone.now()).order_by(
@@ -15,3 +17,8 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(BlogPost, pk=pk)
     return render(request, "blog/post_detail.html", {"post": post})
+
+
+def post_new(request):
+    form = PostForm()
+    return render(request, "blog/post_edit.html", {"form": form})
