ff7ebee738f6ae9a057b48d341cd0049b369aeb6
Pitrified <nobili.pietro@gmail.com>
2020-08-11 11:16:00 +0200

    [PYR] Add text on image

--- desktop-changer/pyramids_creator.py
+++ desktop-changer/pyramids_creator.py
@@ -2,7 +2,9 @@ import argparse
 import logging
 
 from pathlib import Path
-from PIL import Image, ImageDraw
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 from colorsys import hsv_to_rgb
 
 
@@ -70,9 +72,29 @@ def setup_env():
     return args
 
 
-def draw_image(
-    img_path, rgb_sky, rgb_bright, rgb_shade, rgb_background, img_size, img_num=None
-):
+def get_font_size(text, max_hei, font_name="DejaVuSans.ttf"):
+    """Finds the font size so that the text is as big as requested
+
+    https://stackoverflow.com/a/4902713
+    """
+    # logg = logging.getLogger(f"c.{__name__}.get_font_size")
+    # logg.debug("Start get_font_size")
+
+    fontsize = 1
+    font = ImageFont.truetype(font_name, fontsize)
+
+    while font.getsize(text)[1] < max_hei:
+        fontsize += 1
+        font = ImageFont.truetype(font_name, fontsize)
+        # logg.debug(f"fontsize: {fontsize}")
+
+    fontsize -= 1
+    font = ImageFont.truetype(font_name, fontsize)
+
+    return font
+
+
+def draw_image(img_path, rgb_sky, rgb_sunny, rgb_shade, rgb_background, img_size):
     """TODO: what is draw_image doing?
     """
     # logg = logging.getLogger(f"c.{__name__}.draw_image")
@@ -85,11 +107,15 @@ def draw_image(
     draw = ImageDraw.Draw(im)
 
     # box for the pie
-    left = 200
-    top = 200
-    right = 600
-    bottom = 600
-    pie_box = (left, top, right, bottom)
+    pie_x = img_size[0] * 0.1
+    pie_y = img_size[1] * 0.2
+    pie_l = img_size[0] * 0.35
+    # left = 200
+    # top = 200
+    # right = 600
+    # bottom = 600
+    # pie_box = (left, top, right, bottom)
+    pie_box = (pie_x, pie_y, pie_x + pie_l, pie_y + pie_l)
 
     # draw the pie
     slice_1 = 130
@@ -97,13 +123,47 @@ def draw_image(
     slice_3 = 58
     draw.pieslice(pie_box, start=slice_1, end=slice_2, fill=rgb_sky)
     draw.pieslice(pie_box, start=slice_2, end=slice_3, fill=rgb_shade)
-    draw.pieslice(pie_box, start=slice_3, end=slice_1, fill=rgb_bright)
+    draw.pieslice(pie_box, start=slice_3, end=slice_1, fill=rgb_sunny)
+
+    # square dimensions
+    sq_x = img_size[0] * 0.6  # x of the left side of the squares
+    sq_y = img_size[1] * 0.25  # y of the top square
+    sq_l = img_size[0] * 0.05  # size of the squares
+    sq_space = sq_l / 2  # space between the squares
 
     # squares
+    sq_1 = (sq_x, sq_y, sq_x + sq_l, sq_y + sq_l)
+    draw.rectangle(sq_1, fill=rgb_sky)
+    sq_2 = (sq_x, sq_y + sq_l + sq_space, sq_x + sq_l, sq_y + sq_l * 2 + sq_space)
+    draw.rectangle(sq_2, fill=rgb_sunny)
+    sq_3 = (
+        sq_x,
+        sq_y + sq_l * 2 + sq_space * 2,
+        sq_x + sq_l,
+        sq_y + sq_l * 3 + sq_space * 2,
+    )
+    draw.rectangle(sq_3, fill=rgb_shade)
+
+    # font
+    font = get_font_size("Sky", sq_space)
+
+    # text shift
+    te_s = sq_space
 
     # text
+    te_1 = (sq_1[0] + te_s + sq_l, sq_1[1] + sq_space / 2)
+    draw.text(te_1, "Sky", fill="white", font=font)
+    te_2 = (sq_2[0] + te_s + sq_l, sq_2[1] + sq_space / 2)
+    draw.text(te_2, "Sunny side of pyramid", fill="white", font=font)
+    te_3 = (sq_3[0] + te_s + sq_l, sq_3[1] + sq_space / 2)
+    draw.text(te_3, "Shady side of pyramid", fill="white", font=font)
 
     # image number
+    img_num = img_path.name[-6:-4]
+    num_pos = (sq_1[0], sq_1[1] + sq_space * 10)
+    draw.text(num_pos, img_num, fill="white", font=font)
+
+    # TODO: stars at night!
 
     # save the image
     im.save(img_path, quality=95)
@@ -120,13 +180,13 @@ def hsv_to_rgb255(hsv):
     return rgb
 
 
-def hsv180_to_rgb255(hsv180):
-    """TODO: what is hsv180_to_rgb255 doing?
+def hsv360_to_rgb255(hsv360):
+    """TODO: what is hsv360_to_rgb255 doing?
     """
-    # logg = logging.getLogger(f"c.{__name__}.hsv180_to_rgb255")
-    # logg.debug("Start hsv180_to_rgb255")
+    # logg = logging.getLogger(f"c.{__name__}.hsv360_to_rgb255")
+    # logg.debug("Start hsv360_to_rgb255")
 
-    hsv = tuple(c / 180 for c in hsv180)
+    hsv = (hsv360[0] / 360, hsv360[1] / 100, hsv360[2] / 100)
     rgb = hsv_to_rgb(*hsv)
     rgb = tuple(int(c * 255) for c in rgb)
     return rgb
@@ -136,28 +196,28 @@ def get_colors():
     """TODO: what is get_colors doing?
 
     0/6 000 red
-    1/6 030 yellow
-    2/6 060 green
-    3/6 090 cyan
-    4/6 120 blue
-    5/6 150 magenta
+    1/6 060 yellow
+    2/6 120 green
+    3/6 180 cyan
+    4/6 240 blue
+    5/6 300 magenta
     """
     # logg = logging.getLogger(f"c.{__name__}.get_colors")
     # logg.debug("Start get_colors")
 
     hsv_all_sky = {
-        0: (0, 180, 180),
-        1: (30, 180, 180),
-        2: (60, 180, 180),
-        3: (90, 180, 180),
-        4: (120, 180, 180),
-        5: (150, 180, 180),
-        6: (0, 0, 0),
-        7: (0, 0, 0),
-        8: (0, 0, 0),
-        9: (0, 0, 0),
-        10: (0, 0, 0),
-        11: (0, 0, 0),
+        0: (0, 100, 100),
+        1: (30, 100, 100),
+        2: (60, 100, 100),
+        3: (90, 100, 100),
+        4: (120, 100, 100),
+        5: (150, 100, 100),
+        6: (180, 100, 100),
+        7: (210, 100, 100),
+        8: (240, 100, 100),
+        9: (270, 100, 100),
+        10: (300, 100, 100),
+        11: (330, 100, 100),
         12: (0, 0, 0),
         13: (0, 0, 0),
         14: (0, 0, 0),
@@ -172,19 +232,19 @@ def get_colors():
         23: (0, 0, 0),
     }
 
-    hsv_all_bright = {
-        0: (0, 0, 0),
-        1: (0, 0, 0),
-        2: (0, 0, 0),
-        3: (0, 0, 0),
-        4: (0, 0, 0),
-        5: (0, 0, 0),
-        6: (0, 0, 0),
-        7: (0, 0, 0),
-        8: (0, 0, 0),
-        9: (0, 0, 0),
-        10: (0, 0, 0),
-        11: (0, 0, 0),
+    hsv_all_sunny = {
+        0: (0, 50, 100),
+        1: (30, 50, 100),
+        2: (60, 50, 100),
+        3: (90, 50, 100),
+        4: (120, 50, 100),
+        5: (150, 50, 100),
+        6: (180, 50, 100),
+        7: (210, 50, 100),
+        8: (240, 50, 100),
+        9: (270, 50, 100),
+        10: (300, 50, 100),
+        11: (330, 50, 100),
         12: (0, 0, 0),
         13: (0, 0, 0),
         14: (0, 0, 0),
@@ -199,34 +259,9 @@ def get_colors():
         23: (0, 0, 0),
     }
 
-    hsv_all_shade = {
-        0: (0, 0, 0),
-        1: (0, 0, 0),
-        2: (0, 0, 0),
-        3: (0, 0, 0),
-        4: (0, 0, 0),
-        5: (0, 0, 0),
-        6: (0, 0, 0),
-        7: (0, 0, 0),
-        8: (0, 0, 0),
-        9: (0, 0, 0),
-        10: (0, 0, 0),
-        11: (0, 0, 0),
-        12: (0, 0, 0),
-        13: (0, 0, 0),
-        14: (0, 0, 0),
-        15: (0, 0, 0),
-        16: (0, 0, 0),
-        17: (0, 0, 0),
-        18: (0, 0, 0),
-        19: (0, 0, 0),
-        20: (0, 0, 0),
-        21: (0, 0, 0),
-        22: (0, 0, 0),
-        23: (0, 0, 0),
-    }
+    hsv_all_shade = {k: (hsv_all_sunny[k][0], 100, 50) for k in hsv_all_sunny}
 
-    return hsv_all_sky, hsv_all_bright, hsv_all_shade
+    return hsv_all_sky, hsv_all_sunny, hsv_all_shade
 
 
 def run_pyramids_creator(args):
@@ -241,7 +276,7 @@ def run_pyramids_creator(args):
     logg = logging.getLogger(f"c.{__name__}.run_pyramids_creator")
     logg.debug("Starting run_pyramids_creator")
 
-    img_size = (1200, 900)
+    img_size = (1600, 900)
     rgb_background = (12, 12, 12)
 
     img_folder = Path("./out_pyramids")
@@ -249,17 +284,17 @@ def run_pyramids_creator(args):
     if not img_folder.exists():
         img_folder.mkdir(parents=True)
 
-    hsv_all_sky, hsv_all_bright, hsv_all_shade = get_colors()
+    hsv_all_sky, hsv_all_sunny, hsv_all_shade = get_colors()
 
-    for i in range(6):
+    for i in range(12):
         img_name = f"pyr_{i:02d}.jpg"
         img_path = img_folder / img_name
 
-        rgb_sky = hsv180_to_rgb255(hsv_all_sky[i])
-        rgb_bright = hsv180_to_rgb255(hsv_all_bright[i])
-        rgb_shade = hsv180_to_rgb255(hsv_all_shade[i])
+        rgb_sky = hsv360_to_rgb255(hsv_all_sky[i])
+        rgb_sunny = hsv360_to_rgb255(hsv_all_sunny[i])
+        rgb_shade = hsv360_to_rgb255(hsv_all_shade[i])
 
-        draw_image(img_path, rgb_sky, rgb_bright, rgb_shade, rgb_background, img_size)
+        draw_image(img_path, rgb_sky, rgb_sunny, rgb_shade, rgb_background, img_size)
 
 
 if __name__ == "__main__":
