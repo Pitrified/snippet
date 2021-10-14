8bd218350f07779b2b1e20b211447ee4e015acc0
Pitrified <nobilipietro@gmail.com>
2021-10-14 21:24:19 +0200

    [CURSIVE] Add letter b

--- python/cursive-writer/src/cursive_writer/ligature/word_builder.py
+++ python/cursive-writer/src/cursive_writer/ligature/word_builder.py
@@ -93,6 +93,15 @@ def load_letter_dict(thickness: int, data_dir: Path) -> Dict[str, Letter]:
         pf_spline_high=data_dir / let / "a1_h_000.txt",
         thickness=thickness,
     )
+    let = "b"
+    letters_info[let] = Letter(
+        let,
+        left_type="low_up",
+        right_type="high_up",
+        pf_spline_low=data_dir / let / "b0_l_000.txt",
+        pf_spline_high=data_dir / let / "b0_h_001.txt",
+        thickness=thickness,
+    )
     let = "c"
     letters_info[let] = Letter(
         let,
