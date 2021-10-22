8aa65582dd20b67ab4d684f45bfdc33bf6b23193
Pitrified <nobilipietro@gmail.com>
2021-10-22 18:50:00 +0200

    [FYNE] Move the raster in the widget and not in the renderer

--- go/fynesample/image/image4/imagesample.go
+++ go/fynesample/image/image4/imagesample.go
@@ -18,9 +18,6 @@ import (
 // --------------------------------------------------------------------------------
 
 type myRasterRenderer struct {
-	render  *canvas.Raster
-	objects []fyne.CanvasObject
-
 	myRaster *myRaster
 }
 
@@ -32,41 +29,28 @@ func (mrr *myRasterRenderer) Destroy() {
 }
 
 func (mrr *myRasterRenderer) Layout(size fyne.Size) {
-	fmt.Printf("Layout = %+v\n", size)
-	mrr.render.Resize(size)
+	fmt.Printf("[%-5s] Layout = %+v\n", mrr.myRaster.name, size)
+	mrr.myRaster.raster.Resize(size)
 }
 func (mrr *myRasterRenderer) MinSize() fyne.Size {
-	return fyne.NewSize(400, 400)
+	return mrr.myRaster.minSize
 }
 
 func (mrr *myRasterRenderer) Objects() []fyne.CanvasObject {
-	return mrr.objects
+	return []fyne.CanvasObject{mrr.myRaster.raster}
 }
 
 func (mrr *myRasterRenderer) Refresh() {
-	canvas.Refresh(mrr.render)
-}
-
-func (mrr *myRasterRenderer) rasterUpdate(w, h int) image.Image {
-	fmt.Printf("rasterUpdate : w, h = %+v %+v\n", w, h)
-	pixW := w
-	pixH := h
-	img := image.NewRGBA(image.Rect(0, 0, pixW, pixH))
-	bc := mrr.myRaster.backColor
-	draw.Draw(
-		img,
-		img.Bounds(),
-		&image.Uniform{color.RGBA{bc, bc, bc, 255}},
-		image.Point{0, 0},
-		draw.Src,
-	)
-	return img
+	canvas.Refresh(mrr.myRaster)
 }
 
 // --------------------------------------------------------------------------------
 
 type myRaster struct {
 	widget.BaseWidget
+
+	raster  *canvas.Raster
+	minSize fyne.Size
 
 	backColor uint8
 	name      string
@@ -77,15 +61,7 @@ type myRaster struct {
 var _ fyne.Widget = &myRaster{}
 
 func (mr *myRaster) CreateRenderer() fyne.WidgetRenderer {
-	renderer := &myRasterRenderer{myRaster: mr}
-
-	render := canvas.NewRaster(renderer.rasterUpdate)
-	render.ScaleMode = canvas.ImageScalePixels
-
-	renderer.render = render
-	renderer.objects = []fyne.CanvasObject{render}
-
-	return renderer
+	return &myRasterRenderer{myRaster: mr}
 }
 
 // compliant with Mouseable interface
@@ -100,9 +76,33 @@ func (mr *myRaster) MouseUp(ev *desktop.MouseEvent) {
 	fmt.Printf("[%-5s] MouseUp   ev = %+v\n", mr.name, ev)
 }
 
+// update the raster
+func (mr *myRaster) rasterUpdate(w, h int) image.Image {
+	fmt.Printf("[%-5s] rasterUpdate : w, h = %+v %+v\n", mr.name, w, h)
+	pixW := w
+	pixH := h
+	img := image.NewRGBA(image.Rect(0, 0, pixW, pixH))
+	bc := mr.backColor
+	draw.Draw(
+		img,
+		img.Bounds(),
+		&image.Uniform{color.RGBA{bc, bc, bc, 255}},
+		image.Point{0, 0},
+		draw.Src,
+	)
+	return img
+}
+
 // create a new raster
 func newMyRaster(bc uint8, name string) *myRaster {
 	mr := &myRaster{backColor: bc, name: name}
+
+	raster := canvas.NewRaster(mr.rasterUpdate)
+	raster.ScaleMode = canvas.ImageScalePixels
+	mr.raster = raster
+
+	mr.minSize = fyne.NewSize(400, 400)
+
 	mr.ExtendBaseWidget(mr)
 	return mr
 }
@@ -115,22 +115,13 @@ func main() {
 	myApp := app.New()
 	w := myApp.NewWindow("Image test")
 
-	// create the photo app
-	// game := newGame(board)
-	// build the UI
-	// window.SetContent(game.buildUI())
-	// add the link for typed runes
-	// window.Canvas().SetOnTypedRune(game.typedRune)
-	// start the animation
-	// game.animate()
-
 	aRaster := newMyRaster(140, "Left")
 	bRaster := newMyRaster(40, "Right")
 	doubleRaster := container.New(layout.NewGridLayout(2), aRaster, bRaster)
 
 	text1 := canvas.NewText("Hello", color.White)
 	text2 := canvas.NewText("There", color.RGBA{150, 75, 0, 255})
-	text3 := canvas.NewText("(right)", color.White)
+	text3 := canvas.NewText("(right aligned)", color.White)
 	contentTitle := container.New(layout.NewHBoxLayout(), text1, text2, layout.NewSpacer(), text3)
 
 	input := widget.NewEntry()
