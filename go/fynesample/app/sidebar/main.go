aa24c176e0629ed75e739b78e7c123f90fe70cf4
Pitrified <nobilipietro@gmail.com>
2021-10-27 18:15:12 +0200

    [FYNE] Add experiments with card, button, entry and bind

--- go/fynesample/app/sidebar/main.go
+++ go/fynesample/app/sidebar/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 )
@@ -15,17 +17,105 @@ import (
 // --------------------------------------------------------------------------------
 
 type mySidebar struct {
+	a *myApp
+
 	title *widget.Label
+
+	rotate        *widget.Card
+	rotBank       []*widget.Button
+	rotValEntry   *widget.Entry
+	rotValF64     float64
+	rotValBind    binding.ExternalFloat
+	rotValSet     *widget.Button
+	rotDeltaEntry *widget.Entry
+	rotDeltaSet   *widget.Button
 }
 
-func newSidebar() *mySidebar {
-	return &mySidebar{}
+func newSidebar(a *myApp) *mySidebar {
+	return &mySidebar{a: a}
+}
+
+func (s *mySidebar) clickRotBank(id int) {
+	fmt.Printf("Pressed button %+v\n", id)
+	oldValue, err := s.rotValBind.Get()
+	if err != nil {
+		return
+	}
+	s.rotValBind.Set(oldValue + float64(id))
 }
 
 func (s *mySidebar) buildSidebar() *fyne.Container {
-	s.title = widget.NewLabel("The title")
+	s.title = widget.NewLabelWithStyle(
+		"The title",
+		fyne.TextAlignCenter,
+		fyne.TextStyle{Bold: true},
+	)
 
-	return container.NewVBox(s.title)
+	// ----- Bank of buttons -----
+
+	// TODO use a map to use already correct values rot[0]=-10
+	s.rotBank = make([]*widget.Button, 6)
+	for i := 0; i < len(s.rotBank); i++ {
+		c := i
+		s.rotBank[i] = widget.NewButton(
+			fmt.Sprintf("B: %d", i),
+			func() {
+				s.clickRotBank(c)
+			},
+		)
+	}
+	contBank := container.NewHBox(
+		s.rotBank[0],
+		s.rotBank[1],
+		s.rotBank[2],
+		s.rotBank[3],
+		s.rotBank[4],
+		s.rotBank[5],
+	)
+
+	// ----- Entry to rotate by this value -----
+
+	s.rotDeltaEntry = widget.NewEntry()
+	s.rotDeltaEntry.PlaceHolder = "0.0"
+	s.rotDeltaSet = widget.NewButton(
+		"Rotate",
+		func() {
+			str := s.rotDeltaEntry.Text
+			val, err := strconv.ParseFloat(str, 64)
+			if err != nil {
+				fmt.Printf("Failed to parse str = %q\n", str)
+				return
+			}
+			fmt.Printf("Rotated the rot value by %.3f\n", val)
+		},
+	)
+	contDelta := container.NewBorder(nil, nil, nil, s.rotDeltaSet, s.rotDeltaEntry)
+
+	// ----- Entry to set new value -----
+
+	s.rotValBind = binding.BindFloat(&s.rotValF64)
+	s.rotValEntry = widget.NewEntryWithData(binding.FloatToString(s.rotValBind))
+	s.rotValSet = widget.NewButton(
+		"Set",
+		func() {
+			val, err := s.rotValBind.Get()
+			if err != nil {
+				return
+			}
+			fmt.Printf("Set the rot value to %.3f\n", val)
+		},
+	)
+	contVal := container.NewBorder(nil, nil, nil, s.rotValSet, s.rotValEntry)
+
+	// ----- Card with rotate widgets -----
+
+	contCard := container.NewVBox(contBank, contDelta, contVal)
+
+	s.rotate = widget.NewCard("Rotate", "", contCard)
+
+	// vCont := container.NewVBox(s.rotate, contBank)
+	vCont := container.NewVBox(s.title, s.rotate)
+	return vCont
 }
 
 // --------------------------------------------------------------------------------
@@ -58,7 +148,7 @@ func (a *myApp) buildUI() {
 	label2 := widget.NewLabel("(right aligned)")
 	contentTitle := container.New(layout.NewHBoxLayout(), label1, layout.NewSpacer(), label2)
 
-	a.sidebar = newSidebar()
+	a.sidebar = newSidebar(a)
 	contSidebar := a.sidebar.buildSidebar()
 
 	// borderCont := container.New(layout.NewBorderLayout(contentTitle, contentInput, nil, nil),
