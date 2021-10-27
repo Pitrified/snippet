b40ba924c30dcaa1f612dd71d574fb3fd54560e3
Pitrified <nobilipietro@gmail.com>
2021-10-27 19:26:11 +0200

    [FYNE] Use proper callbacks for buttons

--- go/fynesample/app/sidebar/main.go
+++ go/fynesample/app/sidebar/main.go
@@ -35,13 +35,53 @@ func newSidebar(a *myApp) *mySidebar {
 	return &mySidebar{a: a}
 }
 
-func (s *mySidebar) clickRotBank(id int) {
+// Clicked one of the rotate bank buttons
+func (s *mySidebar) rotBankCallback(id float64) {
 	fmt.Printf("Pressed button %+v\n", id)
 	oldValue, err := s.rotValBind.Get()
 	if err != nil {
 		return
 	}
 	s.rotValBind.Set(oldValue + float64(id))
+}
+
+// Clicked the val set button
+func (s *mySidebar) rotValCallback() {
+	val, err := s.rotValBind.Get()
+	if err != nil {
+		return
+	}
+	// the value in rotValBind is already correct: now tell this to the turtle
+	fmt.Printf("Set the rot value to %.3f\n", val)
+}
+
+// Press enter on the val entry
+func (s *mySidebar) rotValSubmitted(_ string) {
+	s.rotValCallback()
+}
+
+// Clicked delta button
+func (s *mySidebar) rotDeltaCallback() {
+	// string in the entry
+	str := s.rotDeltaEntry.Text
+	// convert it to float
+	entryVal, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		fmt.Printf("Failed to parse str = %q\n", str)
+		return
+	}
+	fmt.Printf("Rotated the rot value by %.3f\n", entryVal)
+	// current value of rotation
+	curVal, err := s.rotValBind.Get()
+	if err != nil {
+		return
+	}
+	s.rotValBind.Set(entryVal + curVal)
+}
+
+// Press enter on the delta entry
+func (s *mySidebar) rotDeltaSubmitted(_ string) {
+	s.rotDeltaCallback()
 }
 
 func (s *mySidebar) buildSidebar() *fyne.Container {
@@ -53,67 +93,45 @@ func (s *mySidebar) buildSidebar() *fyne.Container {
 
 	// ----- Bank of buttons -----
 
-	// TODO use a map to use already correct values rot[0]=-10
+	rotVal := map[int]float64{0: -10, 1: -1, 2: -0.1, 3: 0.1, 4: 1, 5: 10}
 	s.rotBank = make([]*widget.Button, 6)
 	for i := 0; i < len(s.rotBank); i++ {
-		c := i
+		c := rotVal[i]
 		s.rotBank[i] = widget.NewButton(
-			fmt.Sprintf("B: %d", i),
-			func() {
-				s.clickRotBank(c)
-			},
+			fmt.Sprintf("%+v", c),
+			func() { s.rotBankCallback(c) },
 		)
 	}
-	contBank := container.NewHBox(
-		s.rotBank[0],
-		s.rotBank[1],
-		s.rotBank[2],
-		s.rotBank[3],
-		s.rotBank[4],
-		s.rotBank[5],
+	contBank := container.NewGridWithColumns(
+		6,
+		s.rotBank[0], s.rotBank[1], s.rotBank[2], s.rotBank[3], s.rotBank[4], s.rotBank[5],
 	)
 
 	// ----- Entry to rotate by this value -----
 
+	deltaLabel := widget.NewLabel("Rotate by:")
 	s.rotDeltaEntry = widget.NewEntry()
-	s.rotDeltaEntry.PlaceHolder = "0.0"
-	s.rotDeltaSet = widget.NewButton(
-		"Rotate",
-		func() {
-			str := s.rotDeltaEntry.Text
-			val, err := strconv.ParseFloat(str, 64)
-			if err != nil {
-				fmt.Printf("Failed to parse str = %q\n", str)
-				return
-			}
-			fmt.Printf("Rotated the rot value by %.3f\n", val)
-		},
-	)
-	contDelta := container.NewBorder(nil, nil, nil, s.rotDeltaSet, s.rotDeltaEntry)
+	s.rotDeltaEntry.Text = "0.0"
+	s.rotDeltaEntry.OnSubmitted = s.rotDeltaSubmitted
+	s.rotDeltaSet = widget.NewButton("Rotate", s.rotDeltaCallback)
+	contDelta := container.NewBorder(nil, nil, deltaLabel, s.rotDeltaSet, s.rotDeltaEntry)
 
 	// ----- Entry to set new value -----
 
+	valLabel := widget.NewLabel("Current:")
 	s.rotValBind = binding.BindFloat(&s.rotValF64)
 	s.rotValEntry = widget.NewEntryWithData(binding.FloatToString(s.rotValBind))
-	s.rotValSet = widget.NewButton(
-		"Set",
-		func() {
-			val, err := s.rotValBind.Get()
-			if err != nil {
-				return
-			}
-			fmt.Printf("Set the rot value to %.3f\n", val)
-		},
-	)
-	contVal := container.NewBorder(nil, nil, nil, s.rotValSet, s.rotValEntry)
+	s.rotValEntry.OnSubmitted = s.rotValSubmitted
+	s.rotValSet = widget.NewButton("Set", s.rotValCallback)
+	contVal := container.NewBorder(nil, nil, valLabel, s.rotValSet, s.rotValEntry)
 
 	// ----- Card with rotate widgets -----
 
 	contCard := container.NewVBox(contBank, contDelta, contVal)
+	s.rotate = widget.NewCard("Orientation", "", contCard)
 
-	s.rotate = widget.NewCard("Rotate", "", contCard)
+	// ----- All the sidebar content -----
 
-	// vCont := container.NewVBox(s.rotate, contBank)
 	vCont := container.NewVBox(s.title, s.rotate)
 	return vCont
 }
