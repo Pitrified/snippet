32cd5119595906974813607d70ebfa79d73ab83b
Pitrified <nobilipietro@gmail.com>
2021-11-04 15:01:15 +0100

    [Firefly] Add TestSendBlinkTo

--- go/blinker/cellfire/world_test.go
+++ go/blinker/cellfire/world_test.go
@@ -103,6 +103,28 @@ func TestValidatePos(t *testing.T) {
 	}
 }
 
+func TestSendBlinkTo(t *testing.T) {
+	w := NewWorld(10, 10, 100)
+
+	// near the right top corner
+	f := NewFirefly(99.5, 99.5, 0, 0, 1000, w)
+	w.SendBlinkTo(f, w.Cells[0][0], 'R')
+	assert.Equal(t, 1, len(w.Cells[1][0].blinkQueue),
+		"The cell to the right should have received the Firefly on the blinkQueue.")
+	w.SendBlinkTo(f, w.Cells[0][0], 'T')
+	assert.Equal(t, 1, len(w.Cells[0][1].blinkQueue),
+		"The cell to the top should have received the Firefly on the blinkQueue.")
+
+	// near the left bottom corner
+	g := NewFirefly(0.5, 0.5, 0, 0, 1000, w)
+	w.SendBlinkTo(g, w.Cells[0][0], 'L')
+	assert.Equal(t, 1, len(w.Cells[9][0].blinkQueue),
+		"The cell to the left should have received the Firefly on the blinkQueue.")
+	w.SendBlinkTo(g, w.Cells[0][0], 'B')
+	assert.Equal(t, 1, len(w.Cells[0][9].blinkQueue),
+		"The cell to the bottom should have received the Firefly on the blinkQueue.")
+}
+
 // Check that the fields/verbs used when printing are valid.
 func TestStringWorld(t *testing.T) {
 	w := NewWorld(10, 10, 100)
