a7d12af15395a5443f83e332cda7a8dbdda581de
Pitrified <nobilipietro@gmail.com>
2022-06-12 14:59:50 +0200

    [ALIGN-EPUB] Inspect more the sentence split

---
+++ python/streamlit-sample/align-epub/sentence_split_fr.py
@@ -0,0 +1,54 @@
+"""Improve sentence split for what i need.
+
+###############################################################################################
+
+(4, 0) C’est qu’en vérité
+(4, 1) –
+(4, 2) il m’est permis de le dire « puisqu’il ne saurait y avoir en tout ceci aucun amour
+(4, 3) -propre d’auteur
+(4, 4) »
+(4, 5) et que je ne fais que transcrire des faits sur lesquels une documentation exceptionnelle me permet d’apporter une lumière nouvelle
+
+(7, 2) Les médecins ne répondent pas de la vie de Mlle Stangerson.
+(7, 3) »
+
+(12, 0) « – il était minuit et demi, nous a raconté ce brave homme ( ?
+(12, 1) )
+(12, 2) , et je me trouvais dans le laboratoire où travaillait encore M. Stangerson quand l’affaire est arrivée.
+(12, 3) J’avais rangé, nettoyé des instruments toute la soirée, et j’attendais le départ de M. Stangerson pour aller me coucher.
+(12, 4) Mlle Mathilde avait travaillé avec son père jusqu’à minuit ;
+(12, 5) les douze coups de minuit sonnés au coucou du laboratoire, elle s’était levée
+(12, 6) , avait embrassé M. Stangerson, lui souhaitant une bonne nuit.
+
+(13, 0) «
+(13, 1) Nous étions donc restés, M. Stangerson et moi, dans le pavillon.
+
+(13, 9) C’était la voix de mademoiselle qui criait : « À l’assassin !
+(13, 10) À l’assassin !
+(13, 11) Au secours !
+(13, 12) » Aussitôt des coups de revolver retentirent et il y eut un grand bruit de tables, de meubles renversés, jetés par terre, comme au cours d’une lutte, et encore la voix de mademoiselle qui criait : « À l’assassin ! …
+(13, 13) Au secours !
+(13, 14) … Papa !
+(13, 15) Papa !
+(13, 16) »
+
+(22, 8) Nous aurions voulu poser bien des questions encore au père Jacques – Jacques-Louis Moustier
+(22, 9) –
+(22, 10) mais on est venu justement le chercher de la part du juge d’instruction qui poursuivait son enquête dans la grande salle du château.
+
+(25, 0) «
+(25, 1) –
+(25, 2) Il nous est impossible, nous répondit M. de Marquet, de dire quoi que ce soit.
+
+(30, 1) espérons
+(30, 2) -le,
+(30, 3) du moins, pour la raison humaine, comme dit le juge d’instruction.
+
+###############################################################################################
+
+(2, 4) « Rouletabille !
+(2, 5) » _
+(2, 6) As
+(2, 7) -tu vu Rouletabille ?
+(2, 8) – Tiens !
+"""
