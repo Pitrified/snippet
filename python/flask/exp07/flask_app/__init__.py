22455df0eece64b3c4ce42e544caf85df7097bb2
Pitrified <nobilipietro@gmail.com>
2022-06-19 00:12:13 +0200

    [FLASK] Move helper func in utils

--- python/flask/exp07/flask_app/__init__.py
+++ python/flask/exp07/flask_app/__init__.py
@@ -2,15 +2,9 @@
 
 from flask import Flask
 
-
-print(f"Initialize Flask app with {__name__=}")
-
 app = Flask(__name__)
-app.secret_key = "any random string"
 
 # global state
 gs = {}
-
-print("__init__.py is going to import routes")
 
 from flask_app import routes
--- python/flask/exp07/flask_app/routes.py
+++ python/flask/exp07/flask_app/routes.py
@@ -3,16 +3,7 @@ from flask import render_template, session
 from transformers.pipelines import pipeline
 
 from flask_app import app, gs
-
-
-def pipe_loader(lt="en", lt_other="fr"):
-    """Load a pipeline and store it in the session object."""
-    print(f"loading {lt} {lt_other}")
-    if "pipeline" not in gs:
-        print(f"loading to session")
-        gs["pipeline"] = pipeline(
-            "translation", model=f"Helsinki-NLP/opus-mt-{lt}-{lt_other}"
-        )
+from flask_app.utils import pipe_loader
 
 
 @app.route("/")
