641d6ba197116ff450192ec3dd7517705718df73
Pitrified <nobilipietro@gmail.com>
2022-06-18 10:28:17 +0200

    [FLASK] Add some experiments with loading files

--- python/flask/exp06/app.py
+++ python/flask/exp06/app.py
@@ -1,12 +1,14 @@
-"""Sample app to load files."""
+"""Sample app to load files.
+
+https://blog.miguelgrinberg.com/post/handling-file-uploads-with-flask
+"""
 import base64
 import io
 
 from livereload import Server
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
 
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -15,22 +17,62 @@ app = Flask(__name__)
 app.debug = True
 
 
-@app.route("/", methods=["GET", "POST"])
-def file_drop():
+# 20Mb for an EPub
+app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024 * 20
+
+
+@app.route("/viaform", methods=["GET", "POST"])
+def file_form():
     """Load a file."""
-    filename = "Load a file!"
+    filename = "Load a file"
     if request.method == "POST":
         print(f"{request=}")
         print(f"{request.form=}")
         print(f"{request.files=}")
 
         uploaded_file = request.files["my-epub-file"]
-        filename = uploaded_file.filename
-        if uploaded_file.filename != "":
-            print(f"got file {uploaded_file.filename=}")
+        if uploaded_file.filename is not None:
+            filename = secure_filename(uploaded_file.filename)
+        else:
+            filename = "None"
+        if filename != "" and filename != "None":
+            print(f"got file {filename=}")
             print(f"got file {uploaded_file.headers=}")
 
+        # return redirect(url_for("file_form"))
+
+    return render_template("fileformer.html", filename=filename)
+
+
+@app.route("/viadrop", methods=["GET", "POST"])
+def file_drop():
+    """Load a file with fancy drops."""
+    filename = "Load something please."
+    if request.method == "POST":
+        print(f"{request=}")
+        print(f"{request.form=}")
+        print(f"{request.files=}")
+
+        file_key = "my-awesome-dropzone-name"
+        if file_key in request.files:
+            uploaded_file = request.files[file_key]
+            if uploaded_file.filename is not None:
+                filename = secure_filename(uploaded_file.filename)
+            else:
+                filename = "None"
+            if filename != "" and filename != "None":
+                print(f"got file {filename=}")
+                print(f"got file {uploaded_file.headers=}")
+        else:
+            filename = "Not a valid key"
+
     return render_template("filedropper.html", filename=filename)
+
+
+@app.route("/")
+def index():
+    """Render the main page of the app."""
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
